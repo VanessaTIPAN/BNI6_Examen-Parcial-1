7162e3923245ca066d4e0af98fe144728cecd5af
Vanessa Tipan <jossva.14tipan@gmail.com>
2022-01-15 11:09:33 -0500

    Tipan Vanessa ejercicio3

--- Ejertres.sh
+++ Ejertres.sh
@@ -1,1 +1,53 @@
 Ejercicio 3
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ pwd
+/c/Users/Vanesa/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ mkdir test1
+mkdir: cannot create directory ‘test1’: File exists
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ tocuh test1.txt
+bash: tocuh: command not found
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ touch test1.txt
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ echo "Hola GBI6" > test2.txt
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ echo "Hola GBI6" > teste1.py
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$  ls -lh test1/
+total 0
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$  ls -l test1
+total 0
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ ls ../test1
+ls: cannot access '../test1': No such file or directory
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$  ls -l ../test1
+ls: cannot access '../test1': No such file or directory
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$  ls -l ./test1
+total 0
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ ls -lh ../test1
+ls: cannot access '../test1': No such file or directory
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$  ls -a ../test1
+ls: cannot access '../test1': No such file or directory
+
