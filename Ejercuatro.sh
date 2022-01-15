6ea02234254c33025ba774de23dc26659335c2bc
Vanessa Tipan <jossva.14tipan@gmail.com>
2022-01-15 12:03:36 -0500

    Tipan Vanessa ejercicio4

--- Ejercuatro.sh
+++ Ejercuatro.sh
@@ -1,1 +1,5 @@
 Ejercicio 4
+link del video 
+<iframe width="560" height="315" src="https://www.youtube.com/embed/41X-dTpNIIo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+https://youtu.be/41X-dTpNIIo (link de you tube opccional-> si no se habre)
