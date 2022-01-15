0a189ed7d6a6c4866e1602daf28c488c16f5e596
Vanessa Tipan <jossva.14tipan@gmail.com>
2022-01-15 10:35:54 -0500

    Tipan Vanessa ejercicio2

--- Ejerdos.sh
+++ Ejerdos.sh
@@ -1,1 +1,20 @@
 Ejercicio 2
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ pwd
+/c/Users/Vanesa/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ mkdir test1
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ touch text1.txt
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ echo "Hola GBI6" > test2.txt
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ print "Hola GBI6" > test1.py
+
+
+Vanesa@DESKTOP-MVA5365 MINGW64 ~/OneDrive/Imágenes/Bioinformatica/CSB-master/git/sandbox/GBI6_Examen-Parcial-1 (main)
+$ ls test1
+ test1.py   text1.txt     test2.txt
